0e92d4e77fca09abc0a5ec9f28caf583d3864368
Pio-Trek <pio-trek@Piotrs-MacBook-Pro.local>
2020-01-19 10:20:54 +0100

    Spring init project

---
+++ src/test/java/io/spring/hibernatemapping/HibernateMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.hibernatemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
